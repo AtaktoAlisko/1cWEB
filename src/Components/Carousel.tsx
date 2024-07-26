23a0f152d0bf7e2f1bd9d0506ad0ac13217d7887
Mikhail <czubaker@Mikhails-MacBook-Air.local>
2024-07-26 03:35:36 +0200

    carousel and oNas are adaptive?

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -22,157 +22,168 @@ const Carousel = () => {
       data-twe-ride="carousel"
     >
       <div
-        className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
-        data-twe-carousel-indicators
+        id="carouselExampleCaptions"
+        className="relative"
+        data-twe-carousel-init
+        data-twe-ride="carousel"
       >
-        <button
-          type="button"
-          data-twe-target="#carouselExampleCaptions"
-          data-twe-slide-to="0"
-          data-twe-carousel-active
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-twe-target="#carouselExampleCaptions"
-          data-twe-slide-to="1"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-twe-target="#carouselExampleCaptions"
-          data-twe-slide-to="2"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-
-      <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
         <div
-          className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-twe-carousel-active
-          data-twe-carousel-item
-          style={{ backfaceVisibility: "hidden" }}
+          className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+          data-twe-carousel-indicators
         >
-          <Image
-            src={firstImg}
-            className="block w-full h-[700px] object-cover" // Fixed dimensions
-            alt="Slide 1"
-          />
-          <div className="absolute inset-x-0 top-1/2 transform -translate-y-3/4 text-center text-white md:block ">
-            <h2 className="text-6xl mt-[400px] mb-10">
-              Управление кредитованием
-            </h2>
-            <p className="text-3xl leading-[50px] mt-[100px] mb-16">
-              Решение позволяет автоматизировать <br /> все бизнес-процессы
-              выдачи, учета и мониторинга кредитных/лизинговых сделок, <br />{" "}
-              обладая при этом возможностью гибкой настройки и <br /> адаптации
-              под специфику конкретной организации.
-            </p>
-            <button className="w-1/5 h-14 bg-slate-700">Связаться</button>
-          </div>
+          <button
+            type="button"
+            data-twe-target="#carouselExampleCaptions"
+            data-twe-slide-to="0"
+            data-twe-carousel-active
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-twe-target="#carouselExampleCaptions"
+            data-twe-slide-to="1"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-twe-target="#carouselExampleCaptions"
+            data-twe-slide-to="2"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 3"
+          ></button>
         </div>
 
-        <div
-          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-twe-carousel-item
-          style={{ backfaceVisibility: "hidden" }}
-        >
-          <Image
-            src={secondImg}
-            className="block w-full h-[700px] object-cover" // Fixed dimensions
-            alt="Slide 2"
-          />
-          <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-            <h2 className="text-6xl mb-10">1C:ERP</h2>
-            <p className="text-3xl leading-10 mb-16">
-              Решение для построения комплексных информационных систем
-              управления деятельностью многопрофильных предприятий, в том числе
-              с технически сложным производством.
-            </p>
-            <button className="w-1/5 h-14 bg-slate-700">Связаться</button>
-          </div>
-        </div>
-
-        <div
-          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-twe-carousel-item
-          style={{ backfaceVisibility: "hidden" }}
-        >
-          <Image
-            src={thirdImg}
-            className="block w-full h-[700px] object-cover" // Fixed dimensions
-            alt="Slide 3"
-          />
-          <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-            <h2 className="text-6xl mb-10">Управление кредитованием</h2>
-            <p className="text-3xl leading-10 mb-16">
-              Решение позволяет автоматизировать <br /> все бизнес-процессы
-              выдачи, учета и мониторинга кредитных/лизинговых сделок, <br />{" "}
-              обладая при этом возможностью гибкой настройки и <br /> адаптации
-              под специфику конкретной организации.
-            </p>
-            <button className="w-1/5 h-14 bg-slate-700">Связаться</button>
-          </div>
-        </div>
-      </div>
-
-      <button
-        className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-twe-target="#carouselExampleCaptions"
-        data-twe-slide="prev"
-      >
-        <span className="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6"
+        <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+          <div
+            className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-twe-carousel-active
+            data-twe-carousel-item
+            style={{ backfaceVisibility: "hidden" }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
+            <Image
+              src={firstImg}
+              className="block w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[90vh] 2xl:h-[100vh] object-cover" // Responsive height
+              alt="Slide 1"
             />
-          </svg>
-        </span>
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Previous
-        </span>
-      </button>
+            <div className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 text-center text-white md:block">
+              <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-5 sm:mt-6 md:mt-8 lg:mt-10 mb-5 sm:mb-6 md:mb-8 lg:mb-10">
+                Управление кредитованием
+              </h2>
+              <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl leading-6 sm:leading-7 md:leading-8 lg:leading-9 xl:leading-10 mt-5 sm:mt-6 md:mt-8 lg:mt-10 mb-5 sm:mb-6 md:mb-8 lg:mb-10">
+                Решение позволяет автоматизировать <br /> все бизнес-процессы выдачи, учета и мониторинга кредитных/лизинговых сделок, <br />
+                обладая при этом возможностью гибкой настройки и <br /> адаптации под специфику конкретной организации.
+              </p>
+              <button className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-10 sm:h-12 md:h-14 lg:h-16 bg-slate-700">Связаться</button>
+            </div>
+          </div>
 
-      <button
-        className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-twe-target="#carouselExampleCaptions"
-        data-twe-slide="next"
-      >
-        <span className="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6"
+          <div
+            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-twe-carousel-item
+            style={{ backfaceVisibility: "hidden" }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            <Image
+              src={secondImg}
+              className="block w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[90vh] 2xl:h-[100vh] object-cover" // Responsive height
+              alt="Slide 2"
             />
-          </svg>
-        </span>
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Next
-        </span>
-      </button>
+            <div className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 text-center text-white md:block">
+              <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-5 sm:mt-6 md:mt-8 lg:mt-10 mb-5 sm:mb-6 md:mb-8 lg:mb-10">
+                1C:ERP
+              </h2>
+              <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl leading-6 sm:leading-7 md:leading-8 lg:leading-9 xl:leading-10 mt-5 sm:mt-6 md:mt-8 lg:mt-10 mb-5 sm:mb-6 md:mb-8 lg:mb-10">
+                Решение для построения комплексных информационных систем
+                управления деятельностью многопрофильных предприятий, в том числе
+                с технически сложным производством.
+              </p>
+              <button className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-10 sm:h-12 md:h-14 lg:h-16 bg-slate-700">Связаться</button>
+            </div>
+          </div>
+
+          <div
+            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-twe-carousel-item
+            style={{ backfaceVisibility: "hidden" }}
+          >
+            <Image
+              src={thirdImg}
+              className="block w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] xl:h-[90vh] 2xl:h-[100vh] object-cover" // Responsive height
+              alt="Slide 3"
+            />
+            <div className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 text-center text-white md:block">
+              <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-5 sm:mt-6 md:mt-8 lg:mt-10 mb-5 sm:mb-6 md:mb-8 lg:mb-10">
+                Управление кредитованием
+              </h2>
+              <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl leading-6 sm:leading-7 md:leading-8 lg:leading-9 xl:leading-10 mt-5 sm:mt-6 md:mt-8 lg:mt-10 mb-5 sm:mb-6 md:mb-8 lg:mb-10">
+                Решение позволяет автоматизировать <br /> все бизнес-процессы
+                выдачи, учета и мониторинга кредитных/лизинговых сделок, <br />
+                обладая при этом возможностью гибкой настройки и <br /> адаптации
+                под специфику конкретной организации.
+              </p>
+              <button className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-10 sm:h-12 md:h-14 lg:h-16 bg-slate-700">Связаться</button>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+          type="button"
+          data-twe-target="#carouselExampleCaptions"
+          data-twe-slide="prev"
+        >
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Previous
+          </span>
+        </button>
+
+        <button
+          className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+          type="button"
+          data-twe-target="#carouselExampleCaptions"
+          data-twe-slide="next"
+        >
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Next
+          </span>
+        </button>
+
+
+      </div>
     </div>
   );
 };

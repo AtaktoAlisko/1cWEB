4b1ef7ffd4d8516f1a34c2e7b9717491c94cf93d
Mikhail <czubaker@Mikhails-MacBook-Air.local>
2024-08-15 19:46:24 +0200

    hi

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     const collapseMenu = document.getElementById("collapseMenu");
@@ -28,22 +27,26 @@ export default function Header() {
 
   return (
     <>
-      <header className={`fixed top-0 left-0 w-full shadow-md font-sans tracking-wide z-50 ${isDarkMode ? "bg-gray-900 text-gray-100" : "bg-white text-gray-900"}`}>
-        <div className={`flex flex-wrap items-center justify-between gap-4 px-10 py-4 min-h-[70px] ${isDarkMode ? "bg-gray-900" : "bg-white"}`}>
+      <header className="fixed top-0 left-0 w-full text-white shadow-md font-sans tracking-wide z-50">
+        <div className="flex flex-wrap items-center justify-between gap-4 px-10 py-4 bg-white min-h-[70px]">
           <a href="javascript:void(0)">
             <Image src="/logo.png" alt="logo" width={144} height={36} />
           </a>
           <div
             id="collapseMenu"
-            className={`lg:flex bg-opacity-85 lg:gap-x-5 ${isDarkMode ? "bg-gray-900 text-gray-100" : "bg-white text-gray-900"} ${isMenuOpen ? "" : "hidden"}`}
+            className={`lg:flex bg-opacity-85 lg:gap-x-5 ${
+              isMenuOpen ? "" : "hidden"
+            }`}
           >
             <button
               id="toggleClose"
-              className={`lg:hidden fixed top-4 right-7 z-[100] w-10 h-10 ${isDarkMode ? "bg-gray-800 text-white" : "bg-white text-black"} flex items-center justify-center rounded ${isMenuOpen ? "" : "hidden"}`}
+              className={`lg:hidden fixed top-4 right-7 z-[100] w-10 h-10 bg-white flex items-center justify-center rounded ${
+                isMenuOpen ? "" : "hidden"
+              }`}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-4 h-4 fill-current"
+                className="w-4 h-4 fill-black"
                 viewBox="0 0 320.591 320.591"
               >
                 <path d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"></path>
@@ -51,7 +54,7 @@ export default function Header() {
               </svg>
             </button>
 
-            <ul className={`lg:flex lg:gap-x-5 max-lg:space-y-3 max-lg:fixed ${isDarkMode ? "max-lg:bg-gray-900 text-gray-100" : "max-lg:bg-white text-gray-900"} max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50`}>
+            <ul className="lg:flex lg:gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
               <li className="mb-6 hidden max-lg:block">
                 <a href="javascript:void(0)">
                   <Image src="/logo.png" alt="logo" width={144} height={36} />
@@ -60,7 +63,7 @@ export default function Header() {
               <li className="max-lg:border-b max-lg:py-3 px-3">
                 <a
                   href="/"
-                  className={`hover:text-[#a2382b] block font-bold text-[15px] ${isDarkMode ? "text-gray-100" : "text-gray-900"}`}
+                  className="hover:text-[#a2382b] text-[#333] block font-bold text-[15px]"
                 >
                   Главная
                 </a>
@@ -68,7 +71,7 @@ export default function Header() {
               <li className="max-lg:border-b max-lg:py-3 px-3">
                 <a
                   href="/aboutus"
-                  className={`hover:text-[#a2382b] block font-bold text-[15px] ${isDarkMode ? "text-gray-100" : "text-gray-900"}`}
+                  className="hover:text-[#a2382b] text-[#333] block font-bold text-[15px]"
                 >
                   О нас
                 </a>
@@ -76,7 +79,7 @@ export default function Header() {
               <li className="max-lg:border-b max-lg:py-3 px-3">
                 <a
                   href="javascript:void(0)"
-                  className={`hover:text-[#a2382b] block font-bold text-[15px] ${isDarkMode ? "text-gray-100" : "text-gray-900"}`}
+                  className="hover:text-[#a2382b] text-[#333] block font-bold text-[15px]"
                 >
                   Продукты
                 </a>
@@ -84,7 +87,7 @@ export default function Header() {
               <li className="max-lg:border-b max-lg:py-3 px-3">
                 <a
                   href="javascript:void(0)"
-                  className={`hover:text-[#a2382b] block font-bold text-[15px] ${isDarkMode ? "text-gray-100" : "text-gray-900"}`}
+                  className="hover:text-[#a2382b] text-[#333] block font-bold text-[15px]"
                 >
                   Услуги
                 </a>
@@ -92,7 +95,7 @@ export default function Header() {
               <li className="max-lg:border-b max-lg:py-3 px-3">
                 <a
                   href="javascript:void(0)"
-                  className={`hover:text-[#a2382b] block font-bold text-[15px] ${isDarkMode ? "text-gray-100" : "text-gray-900"}`}
+                  className="hover:text-[#a2382b] text-[#333] block font-bold text-[15px]"
                 >
                   Выполненные проекты
                 </a>
@@ -100,30 +103,22 @@ export default function Header() {
               <li className="max-lg:border-b max-lg:py-3 px-3">
                 <a
                   href="/contact"
-                  className={`hover:text-[#a2382b] block font-bold text-[15px] ${isDarkMode ? "text-gray-100" : "text-gray-900"}`}
+                  className="hover:text-[#a2382b] text-[#333] block font-bold text-[15px]"
                 >
                   Контакты
                 </a>
               </li>
             </ul>
           </div>
-          
-          <div className="flex max-lg:ml-auto">
-            {/* Dark Mode Toggle Button */}
-            <button
-              onClick={() => setIsDarkMode(!isDarkMode)}
-              className={`ml-4 p-2 rounded ${isDarkMode ? "bg-gray-700 text-white" : "bg-gray-200 text-black"}`}
-            >
-              {isDarkMode ? "Light Mode" : "Dark Mode"}
-            </button>
 
+          <div className="flex max-lg:ml-auto">
             <button
               id="toggleOpen"
               className={`lg:hidden ${isMenuOpen ? "hidden" : ""}`}
             >
               <svg
-                className={`w-7 h-7 ${isDarkMode ? "text-white" : "text-black"}`}
-                fill="currentColor"
+                className="w-7 h-7"
+                fill="#000"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -140,7 +135,7 @@ export default function Header() {
 
       {isMenuOpen && (
         <div
-          className={`fixed top-0 left-0 w-full h-full ${isDarkMode ? "bg-black bg-opacity-70" : "bg-black bg-opacity-50"} backdrop-blur-sm z-40`}
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-blur-sm z-40"
           onClick={() => setIsMenuOpen(false)}
         ></div>
       )}

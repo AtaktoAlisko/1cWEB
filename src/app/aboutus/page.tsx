1e0f753629bd1d108f20721f203a7bccccb909af
Mikhail <czubaker@Mikhails-MacBook-Air.local>
2024-08-21 20:13:03 +0200

    Quick fix

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import Header from "@/Components/Header";
 // import Head from "@/Components/ContactBar";
-import Carousel from "@/Components/Carousel";
-import EmojiGrid from "@/Components/EmojiGrid";
+import Carousel from "@/Components/Homepage/Carousel";
+import EmojiGrid from "@/Components/Homepage/EmojiGrid";
 import ProudNumbersBar from "@/Components/Homepage/ProudNumbersBar";
-import BasicInfo from "@/Components/Homepage/BasicInfo";
 import Reviews from "@/Components/Homepage/Reviews";
 import Footer from "@/Components/Footer";
 import ServicesCards from "@/Components/Homepage/ServicesCards";
@@ -15,7 +14,6 @@ export default async function Aboutus() {
     <>
       {/* <ContactBar /> */}
       {/* <Header /> added in layout.tsx */}
-      <BasicInfo />
       <h2 className="text-center text-4xl mt-10 mb-10">ПОЧЕМУ ВЫБИРАЮТ НАС</h2>
       <EmojiGrid />
       <ServicesCards />
